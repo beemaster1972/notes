1c82494fb619d32e6bd9cb92c97de8aaa08f735c
beemaster1972 <ugor.finno@yandex.ru>
2024-03-01 18:07:20 +0500

    Continue implements edit text module

--- service/editnote.py
+++ service/editnote.py
@@ -1,13 +1,25 @@
+from datetime import datetime
+
 from model.note import Note
+from model.notes import Notes
 from service.notesoperation import Operation
+from service.textedit import TextEdit
 
 
 class EditNote(Operation):
-    def operation(self, note: Note) -> Note:
+
+    def __init__(self, notes: Notes):
+        self.notes = notes
+
+    def operation(self, note: Note) -> (Note,):
         """
         Метод редактирующий заметку
         :param note: заметка для редактирования
         :type Note:
-        :return: отредактированную заметку
+        :return: кортеж отредактированную заметку и словарь заметок
         :rtype: Note
         """
+        note = TextEdit().edit_note(note)
+        note.date_modify = datetime.now()
+        self.notes[str(note.date_create.date())] = note
+        return note, self.notes
